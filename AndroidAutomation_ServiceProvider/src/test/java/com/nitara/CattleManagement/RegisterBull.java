edc609ab57e15107c2e2a4acc37e82a8e116bea2
PranavRai05 <pranav.raibryandanielson@gmail.com>
2022-06-07 12:26:52 +0530

    Update RegisterBull.java

--- AndroidAutomation_ServiceProvider/src/test/java/com/nitara/CattleManagement/RegisterBull.java
+++ AndroidAutomation_ServiceProvider/src/test/java/com/nitara/CattleManagement/RegisterBull.java
@@ -10,7 +10,6 @@ import appCommonClasses.GenericBase;
 
 public class RegisterBull extends GenericBase{
 	
-	
 	@Test(dataProvider = "getData",dataProviderClass = DataProviderUtils.class)
 	public void RegisterCattle_Bull(Map<String,String> data) throws Exception {
 		
@@ -29,6 +28,7 @@ public class RegisterBull extends GenericBase{
 		searchFarmPage.enterPhoneNumber(phone);
 		searchFarmPage.pressSearchButton();
 		searchFarmPage.select_farm(phone);
+		searchFarmPage.waitForPageLoadBar();
 		
 		cattleTypePage.select_cattleType("BULL");	
 		
@@ -51,8 +51,332 @@ public class RegisterBull extends GenericBase{
 		helperFunctions.submitData();
 
 	}
+	
+	@Test(groups = {"Regression"}, dataProvider = "getData",dataProviderClass = DataProviderUtils.class)
+	public void RegisterCattle_BullTagNumberAccceptsSpecialCharacters(Map<String,String> data) throws Exception{
+		
+		/**Login **/
+		new Login().Login_ValidData();
+		
+		SPHomePage.pressRegisterCattleButton();
+		
+		//Search for farm
+		String phone = prop.getProperty("FarmerPhone");
+		searchFarmPage.enterPhoneNumber(phone);
+		searchFarmPage.pressSearchButton();
+		searchFarmPage.select_farm(phone);
+		searchFarmPage.waitForPageLoadBar();
+		
+		cattleTypePage.select_cattleType("BULL");	
+		
+		registerCattleBullPage.assert_CattleType();
+		registerCattleBullPage.assert_Phone_Number(phone);
+		registerCattleBullPage.enter_TagNumber(data.get("tagNumber"));
+		
+		
+		/**Entering TagNumber with special characters*/
+		registerCattleBullPage.enter_TagNumber(data.get("tagNumber"));
+		registerCattleBullPage.press_SaveButton();
+		
+		/** Assert Warning Message */
+		WarningPage.captureScreenshots("RegisterBull");
+		WarningPage.assertWarningMsg("Name/Number should be only Alphabets or Numbers.");
+	    
+		
+	}
 
+	@Test(groups = {"Regression"}, dataProvider = "getData",dataProviderClass = DataProviderUtils.class)
+	public void RegisterCattle_BullSameEarTag(Map<String,String> data) throws Exception{
+		
+		/**Login **/
+		new Login().Login_ValidData();
+		
+		SPHomePage.pressRegisterCattleButton();
+		
+		//Search for farm
+		String phone = prop.getProperty("FarmerPhone");
+		searchFarmPage.enterPhoneNumber(phone);
+		searchFarmPage.pressSearchButton();
+		searchFarmPage.select_farm(phone);
+		searchFarmPage.waitForPageLoadBar();
+	
+		/** Bull Register form */
+		cattleTypePage.select_cattleType("BULL");
+		registerCattleBullPage.assert_CattleType();
+		
+		/**Entering Already Registered TagNumber*/
+		registerCattleBullPage.enter_TagNumber(data.get("tagNumber"));
+		registerCattleBullPage.select_YOB(data.get("yearOfBirth"));
+		registerCattleBullPage.press_SaveButton();
+		
+		/** Assert Warning Message */
+		WarningPage.captureScreenshots("RegisterBull");
+		WarningPage.assertWarningMsg("Cattle with entered Name/Number exists. Please enter valid Name/Number.");
+    }
+	
+	@Test(groups = {"Regression"}, dataProvider = "getData",dataProviderClass = DataProviderUtils.class)
+	public void RegisterCattle_BullCoopTagNumberAcceptsNonNumericValue(Map<String,String> data) throws Exception{
+    	
+		/**Login **/
+		new Login().Login_ValidData();
+		
+		
+		String tagNumber = generateData.generateRandomNumber(7);
+		
+		SPHomePage.pressRegisterCattleButton();
+		
+		//Search for farm
+		String phone = prop.getProperty("FarmerPhone");
+		searchFarmPage.enterPhoneNumber(phone);
+		searchFarmPage.pressSearchButton();
+		searchFarmPage.select_farm(phone);
+		searchFarmPage.waitForPageLoadBar();
+	
+		/** Bull Register form */
+		cattleTypePage.select_cattleType("BULL");
+		registerCattleBullPage.assert_CattleType();
+		registerCattleBullPage.enter_TagNumber(tagNumber);
+		
+		/**Entering CoopTagNumber having non numeric values*/
+		registerCattleBullPage.enter_CoopTagNumber(data.get("cooptagNumber"));
+		registerCattleBullPage.press_SaveButton();
+		
+		/** Assert Warning Message */
+		WarningPage.captureScreenshots("RegisterBull");
+		WarningPage.assertWarningMsg("Pashu Aadhar should only be Numeric.Please check and re-enter Pashu Aadhar.");
 
+    }
+	
+
+	@Test(groups = {"Regression"}, dataProvider = "getData",dataProviderClass = DataProviderUtils.class)
+	public void RegisterCattle_BullPashuAadharLesserThan12Digits(Map<String,String> data) throws Exception{
+		
+		/**Login **/
+		new Login().Login_ValidData();	
+		
+		String tagNumber = generateData.generateRandomNumber(7);
+		
+		SPHomePage.pressRegisterCattleButton();
+		
+		//Search for farm
+		String phone = prop.getProperty("FarmerPhone");
+		searchFarmPage.enterPhoneNumber(phone);
+		searchFarmPage.pressSearchButton();
+		searchFarmPage.select_farm(phone);
+		searchFarmPage.waitForPageLoadBar();
+	
+		/** Bull Register form */
+		cattleTypePage.select_cattleType("BULL");
+		registerCattleBullPage.assert_CattleType();
+		registerCattleBullPage.enter_TagNumber(tagNumber);
+		
+		/**Entering CoopTagNumber having non numeric values*/
+		registerCattleBullPage.enter_CoopTagNumber(data.get("cooptagNumber"));
+		registerCattleBullPage.press_SaveButton();
+		Thread.sleep(3000);
+		
+		/** Assert Warning Message */
+		WarningPage.captureScreenshots("RegisterBull");
+		WarningPage.assertWarningMsg("Pashu Aadhar is not valid. Please check and re-enter valid 12-digit Pashu Aadhar Number.");
+ 
+    }
+	
+	
+	@Test(groups = {"Regression"}, dataProvider = "getData",dataProviderClass = DataProviderUtils.class)
+	public void RegisterCattle_BullPashuAadharMoreThan12Digits(Map<String,String> data) throws Exception{
+		
+		/**Login **/
+		new Login().Login_ValidData();
+		
+		String tagNumber = generateData.generateRandomNumber(7);
+		
+		SPHomePage.pressRegisterCattleButton();
+		
+		//Search for farm
+		String phone = prop.getProperty("FarmerPhone");
+		searchFarmPage.enterPhoneNumber(phone);
+		searchFarmPage.pressSearchButton();
+		searchFarmPage.select_farm(phone);
+		searchFarmPage.waitForPageLoadBar();
+	
+		/** Bull Register form */
+		cattleTypePage.select_cattleType("BULL");
+		registerCattleBullPage.assert_CattleType();
+		registerCattleBullPage.enter_TagNumber(tagNumber);
+		
+		/**Entering CoopTagNumber having non numeric values*/
+		registerCattleBullPage.enter_CoopTagNumber(data.get("cooptagNumber"));
+		registerCattleBullPage.press_SaveButton();
+		
+		/** Assert Warning Message */
+		WarningPage.captureScreenshots("RegisterBull");
+		WarningPage.assertWarningMsg("Pashu Aadhar is not valid. Please check and re-enter valid 12-digit Pashu Aadhar Number.");
+
+    }
+	
+	
+	@Test(groups = {"Regression"}, dataProvider = "getData",dataProviderClass = DataProviderUtils.class)
+	public void RegisterCattle_BullCattleTagMandatory(Map<String,String> data) throws Exception{
+		/**Login **/
+		new Login().Login_ValidData();
+
+		SPHomePage.pressRegisterCattleButton();
+		
+		//Search for farm
+		String phone = prop.getProperty("FarmerPhone");
+		searchFarmPage.enterPhoneNumber(phone);
+		searchFarmPage.pressSearchButton();
+		searchFarmPage.select_farm(phone);
+		searchFarmPage.waitForPageLoadBar();
+    		
+		/** Bull Register form */
+    	cattleTypePage.select_cattleType("BULL");
+    	registerCattleBullPage.assert_CattleType();
+    	registerCattleBullPage.press_SaveButton();
+        
+    	/** Assert Warning Message */
+    	WarningPage.captureScreenshots("RegisterBull");
+    	WarningPage.assertWarningMsg("Cattle Name/Number must be between 1-10 characters.");
+    }
+	
+	
+	@Test(groups = {"Regression"}, dataProvider = "getData",dataProviderClass = DataProviderUtils.class)
+    public void RegisterCattle_BullYearOfBirthMandatory(Map<String,String> data) throws Exception{
+		
+		/**Login **/
+		new Login().Login_ValidData();
+		
+		String tagNumber = generateData.generateRandomNumber(7);
+		String cooptagNumber = generateData.generateRandomNumber(12);
+
+		SPHomePage.pressRegisterCattleButton();
+		
+		//Search for farm
+		String phone = prop.getProperty("FarmerPhone");
+		searchFarmPage.enterPhoneNumber(phone);
+		searchFarmPage.pressSearchButton();
+		searchFarmPage.select_farm(phone);
+		searchFarmPage.waitForPageLoadBar();
+	
+		/** Bull Register form */
+		cattleTypePage.select_cattleType("BULL");
+		registerCattleBullPage.assert_CattleType();
+		registerCattleBullPage.enter_TagNumber(tagNumber);
+		registerCattleBullPage.enter_CoopTagNumber (cooptagNumber);
+		registerCattleBullPage.press_SaveButton();
+		
+		/** Assert Warning Message */
+		WarningPage.captureScreenshots("RegisterBull");
+		WarningPage.assertWarningMsg("The field Year of Birth cannot be empty");
+ 
+    }
+	
+	@Test(groups = {"Regression"}, dataProvider = "getData",dataProviderClass = DataProviderUtils.class)
+	public void RegisterCattle_BullMinimumWeight(Map<String,String> data) throws Exception{
+
+		/**Login **/
+		new Login().Login_ValidData();
+		
+		String tagNumber = generateData.generateRandomNumber(7);
+		String cooptagNumber = generateData.generateRandomNumber(12);
+
+		SPHomePage.pressRegisterCattleButton();
+		
+		//Search for farm
+		String phone = prop.getProperty("FarmerPhone");
+		searchFarmPage.enterPhoneNumber(phone);
+		searchFarmPage.pressSearchButton();
+		searchFarmPage.select_farm(phone);
+		searchFarmPage.waitForPageLoadBar();
+	
+		/** Bull Register form */
+		cattleTypePage.select_cattleType("BULL");
+		registerCattleBullPage.assert_CattleType();
+		registerCattleBullPage.enter_TagNumber(tagNumber);
+		registerCattleBullPage.enter_CoopTagNumber (cooptagNumber);
+		registerCattleBullPage.select_YOB(data.get("yearOfBirth"));
+		registerCattleBullPage.select_month(data.get("monthOfBirth"));
+		registerCattleBullPage.select_cattleType(data.get("cattleType"));
+		registerCattleBullPage.select_cattleBreed(data.get("breed"));
+		
+		/**Entering weight of bull less than 15kgs*/
+		registerCattleBullPage.enter_weight(data.get("weight"));
+		registerCattleBullPage.press_SaveButton();
+		
+		/** Assert Warning Message */
+		WarningPage.captureScreenshots("RegisterBull");
+		WarningPage.assertWarningMsg("Weight cannot be less than 15 kg. Please check weight and re-enter weight again.");
+    }
+    
+	@Test(groups = {"Regression"}, dataProvider = "getData",dataProviderClass = DataProviderUtils.class)
+	public void RegisterCattle_BullMaximumWeight(Map<String,String> data) throws Exception{
+		
+		/**Login **/
+		new Login().Login_ValidData();
+		
+		String tagNumber = generateData.generateRandomNumber(7);
+		String cooptagNumber = generateData.generateRandomNumber(12);
+
+		SPHomePage.pressRegisterCattleButton();
+		
+		//Search for farm
+		String phone = prop.getProperty("FarmerPhone");
+		searchFarmPage.enterPhoneNumber(phone);
+		searchFarmPage.pressSearchButton();
+		searchFarmPage.select_farm(phone);
+		searchFarmPage.waitForPageLoadBar();
+	
+		/** Bull Register form */
+		cattleTypePage.select_cattleType("BULL");
+		registerCattleBullPage.assert_CattleType();
+		registerCattleBullPage.enter_TagNumber(tagNumber);
+		registerCattleBullPage.enter_CoopTagNumber (cooptagNumber);
+		registerCattleBullPage.select_YOB(data.get("yearOfBirth"));
+		registerCattleBullPage.select_month(data.get("monthOfBirth"));
+		registerCattleBullPage.select_cattleType(data.get("cattleType"));
+		registerCattleBullPage.select_cattleBreed(data.get("breed"));
+		
+		/**Entering weight of bull more than 1000kgs*/
+		registerCattleBullPage.enter_weight(data.get("weight"));
+		registerCattleBullPage.press_SaveButton();
+		
+		/** Assert Warning Message */
+		WarningPage.captureScreenshots("RegisterBull");
+		WarningPage.assertWarningMsg("Weight cannot be more than 2000 kg. Please check weight and re-enter weight again.");
+    }
+    
+	@Test(groups = {"Regression"}, dataProvider = "getData",dataProviderClass = DataProviderUtils.class)
+	public void RegisterCattle_BullUnder6Months(Map<String,String> data) throws Exception{
+    	
+		/**Login **/
+		new Login().Login_ValidData();
+		
+		String tagNumber = generateData.generateRandomNumber(7);
+		String cooptagNumber = generateData.generateRandomNumber(12);
+
+		SPHomePage.pressRegisterCattleButton();
+		
+		//Search for farm
+		String phone = prop.getProperty("FarmerPhone");
+		searchFarmPage.enterPhoneNumber(phone);
+		searchFarmPage.pressSearchButton();
+		searchFarmPage.select_farm(phone);
+		searchFarmPage.waitForPageLoadBar();
+	
+		/** Bull Register form */
+		cattleTypePage.select_cattleType("BULL");
+		registerCattleBullPage.assert_CattleType();
+		registerCattleBullPage.enter_TagNumber(tagNumber);
+		registerCattleBullPage.enter_CoopTagNumber (cooptagNumber);
+		registerCattleBullPage.select_YOB(data.get("yearOfBirth"));
+		registerCattleBullPage.select_month(data.get("monthOfBirth"));
+		registerCattleBullPage.press_SaveButton();
+		
+		/** Assert Warning Message */
+		WarningPage.captureScreenshots("RegisterBull");
+		WarningPage.assertWarningMsg("Animal cannot be under 6 months old to be a Bull. Please register animal as Calf.");
+    }
+    
 //	//@Test
 //	public void registerBull_Localisation() throws Exception {
 //		
