ac7c5252512c4886ff5e725f9f4d3c87bc01ba70
Eesha <martiseesha@gmail.com>
2022-06-29 12:00:13 +0530

    Add PD Testcases

--- AndroidAutomation_ServiceProvider/src/test/java/com/nitara/BreedingManagement/AddPD.java
+++ AndroidAutomation_ServiceProvider/src/test/java/com/nitara/BreedingManagement/AddPD.java
@@ -27,7 +27,6 @@ public class AddPD extends GenericBase{
 		RegisterMilkingCattle reg = new RegisterMilkingCattle();
 		String coopTag = reg.registerMilkingOrDryCattle(url,"RegisterMilkingCattle_Inseminated");
 		
-		
 		//helperFunctions.Insemination_ForGivenDate(coopTag,new GenerateData().getPastDate(283));
 		
 		new Helper().goTo_AddBreedingActivityScreenFromBasicCP(coopTag,"pd");
@@ -43,5 +42,101 @@ public class AddPD extends GenericBase{
 		
 		/** Submit data and assert success message */
 		new HelperFunctions().submitData();
+	}
+	
+	@Test(groups="Regression",dataProvider = "getData",dataProviderClass = DataProviderUtils.class)
+	public void PD_NoInseminationData(Map<String,String> data) throws Exception {
+	
+	/**Login **/
+	new Login().Login_ValidData();
+	
+	/** Register cattle */
+	String url = prop.getProperty("APIbaseUrl");
+	RegisterMilkingCattle reg = new RegisterMilkingCattle();
+	String coopTag = reg.registerMilkingOrDryCattle(url,"RegisterMilkingOrDryCattle");
+	
+	new Helper().goTo_AddBreedingActivityScreenFromBasicCP(coopTag,"pd");
+	
+	/** Assert warning */
+	addPDPage.assertWarningMsg(data.get("warningMessage"));
+	}
+	
+	@Test(groups="Regression",dataProvider = "getData",dataProviderClass = DataProviderUtils.class)
+	public void PD_PDdateWithin21DaysOfInseminationdate(Map<String,String> data) throws Exception {
+	
+	/**Login **/
+	new Login().Login_ValidData();
+	
+	/** Register cattle */
+	String url = prop.getProperty("APIbaseUrl");
+	RegisterMilkingCattle reg = new RegisterMilkingCattle();
+	String coopTag = reg.registerMilkingOrDryCattle(url,"RegisterMilkingCattle_Inseminated");
+	
+	new Helper().goTo_AddBreedingActivityScreenFromBasicCP(coopTag,"pd");
+	
+	/** Fill PD form */
+	addPDPage.enter_PDdate(data.get("pregnancyTestDate"));
+	addPDPage.press_SaveButton();
+	
+	/** Assert warning */
+	addPDPage.assertWarningMsg(data.get("warningMessage"));
+	}
+	
+	@Test(groups="Regression",dataProvider = "getData",dataProviderClass = DataProviderUtils.class)
+	public void PD_PDdateWithin21DaysOfPDdate(Map<String,String> data) throws Exception {
+	
+	/**Login **/
+	new Login().Login_ValidData();
+	
+	/** Register cattle */
+	String url = prop.getProperty("APIbaseUrl");
+	RegisterMilkingCattle reg = new RegisterMilkingCattle();
+	String coopTag = reg.registerMilkingOrDryCattle(url,"RegisterMilkingCattle_Inseminated");
+	
+	new Helper().goTo_AddBreedingActivityScreenFromBasicCP(coopTag,"pd");
+	
+	/** Fill PD form */
+	addPDPage.enter_PDdate(data.get("pregnancyTestDate"));
+	addPDPage.press_SaveButton();
+	
+	/** Submit data and assert success message */
+	breedingSuccessPage.pressSubmitButton();
+	new HelperFunctions().submitData();
+	
+	new Helper().goTo_AddBreedingActivityScreenFromBasicCP(coopTag,"pd");
+	
+	/** Fill another PD form */
+	addPDPage.enter_PDdate(data.get("SecondpregnancyTestDate"));
+	addPDPage.press_SaveButton();	
+	
+	/** Assert warning */
+	addPDPage.assertWarningMsg(data.get("warningMessage"));
+	}
+	
+	@Test(groups="Regression",dataProvider = "getData",dataProviderClass = DataProviderUtils.class)
+	public void PD_recording2PDActivitiesInOfflineMode(Map<String,String> data) throws Exception {
+	
+	/**Login **/
+	new Login().Login_ValidData();
+	
+	/** Register cattle */
+	String url = prop.getProperty("APIbaseUrl");
+	RegisterMilkingCattle reg = new RegisterMilkingCattle();
+	String coopTag = reg.registerMilkingOrDryCattle(url,"RegisterMilkingCattle_Inseminated");
+	
+	new Helper().goTo_AddBreedingActivityScreenFromBasicCP(coopTag,"pd");
+	
+	/** Fill PD form */
+	addPDPage.enter_PDdate(data.get("pregnancyTestDate"));
+	addPDPage.press_SaveButton();
+	
+	/** Fill another PD form */
+	breedingSuccessPage.recordAnotherActivity();
+	breedingActivityListPage.select_pd();
+	addPDPage.enter_PDdate(data.get("SecondpregnancyTestDate"));
+	addPDPage.press_SaveButton();	
+	
+	/** Assert warning */
+	addPDPage.assertWarningMsg(data.get("warningMessage"));
 	}
 }
